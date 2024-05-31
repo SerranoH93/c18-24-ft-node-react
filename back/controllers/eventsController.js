0489998e38da8e420b58a1e4e247e5be1a78e31d
saorregog <saorregog@gmail.com>
2024-05-31 09:53:34 -0500

    implementacion de uuid en base de datos, y endpoints de creacion y recuperacion de evento

--- back/controllers/eventsController.js
+++ back/controllers/eventsController.js
@@ -1,18 +1,10 @@
+import crypto from "node:crypto";
 import prisma from "../utils/prisma.js";
 import uploadImage from "../utils/cloudinary.js";
 
 export const createEvent = async (req, res) => {
   try {
-    // Validate if the event name already exists. This must be done before the event creation because the event_poster_url has to be provided by a successful image upload and is required as a key in the req object
-    const createdEvent = await prisma.events.findUnique({
-      where: { name: req.body.name },
-    });
-
-    await prisma.$disconnect();
-
-    if (createdEvent) {
-      return res.status(409).json({ message: "Evento ya existe" });
-    }
+    req.body.uuid = crypto.randomUUID();
 
     if (req.files) {
       const uploadResult = await uploadImage(
@@ -71,12 +63,12 @@ export async function getAllEventsPaginated(req, res) {
   }
 }
 
-export async function retrieveEventById(req, res) {
-  const { id } = req.params;
+export async function retrieveEventByUUID(req, res) {
+  const { uuid } = req.params;
 
   try {
     const eventData = await prisma.events.findUnique({
-      where: { id: parseInt(id) },
+      where: { uuid: uuid },
     });
 
     await prisma.$disconnect();
