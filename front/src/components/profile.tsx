7ec6c1b5b062fd85f04d55ae510545c1de75821e
Alejandro Serrano <92839762+SerranoH93@users.noreply.github.com>
2024-05-31 17:55:32 -0600

    Se agrega secciones para los eventos

--- front/src/components/profile.tsx
+++ front/src/components/profile.tsx
@@ -4,11 +4,11 @@ export default function Profile() {
     const user = userAuthStore((state) => state.user);
 
     return (
-
-        <div>
-            <img src={user?.avatar_url} alt="User avatar" />
+        <div className="flex flex-row gap-1">
+            <button><img src="/ticket.svg" alt="" /></button>
+            <button><img src="/favorite.svg" alt="" /></button>
+            <button><img src="/notifications.svg" alt="" /></button>
+            <button><img className="w-11 h-11 border border-solid border-black rounded-full" src={user?.avatar_url} alt="User avatar"/></button>            
         </div>
-        
-
     )
 }
